767354873c7b7284c8d2b8e2c56aaed0510b2047
Lucas <lucfr1991@gmail.com>
2026-05-14 20:02:35 -0300

    feat: implementar componente de lista de fatos

--- src/components/FactList.tsx
+++ src/components/FactList.tsx
@@ -1,20 +1,20 @@
 import type { Fact } from '../types';
-import { FactItem } from './FactItem';
+import FactItem from './FactItem';
 
 interface FactListProps {
-  facts: Fact[];
+  facts: Fact[]
 };
 
-export function FactList({ facts }: FactListProps) {
+export default function FactList({ facts }: FactListProps) {
   if (facts.length === 0) {
-    return <p>Nenhum fato encontrado para esta categoria.</p>;
+    return <p>Nenhum fato para esta categoria ainda. Crie o primeiro!✌️</p>
   };
 
   return (
-    <ul>
-      {facts.map(fact => (
-        <FactItem key={fact.id} fact={fact} />
-      ))}
-    </ul>
+    <>
+      { facts.map(fact => {
+        return <FactItem key={fact.id} fact={fact} />
+      }) }
+    </>
   );
-}
+};
