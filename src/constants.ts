967583820fc7b0b9955ef8099b126ccbb044f601
unknown <lucasoliveira0472@digitalcollege.internal>
2026-05-14 19:29:10 -0300

    atualizar3

--- src/constants.ts
+++ src/constants.ts
@@ -2,34 +2,34 @@ import type { Category } from "./types";
 
 export const CATEGORIES: Category[] = [{
   value: 'technology',
-  label: 'Tecnologia',
+  label: 'tecnologia',
   color: 'bg-blue-500'
 }, {
   value: 'science',
-  label: 'Ciência',
+  label: 'ciência',
   color: 'bg-green-500'
 }, {
   value: 'finance',
-  label: 'Finanças',
+  label: 'finança',
   color: 'bg-red-500'
 }, {
   value: 'society',
-  label: 'Sociedade',
-  color: 'bg-yellow-400'
+  label: 'sociedade',
+  color: 'bg-yellow-500'
 }, {
   value: 'entertainment',
-  label: 'Entretenimento',
-  color: 'bg-purple-500'
-}, {
-  value: 'health',
-  label: 'Saúde',
+  label: 'entretenimento',
   color: 'bg-pink-500'
 }, {
+  value: 'health',
+  label: 'saúde',
+  color: 'bg-teal-500'
+}, {
   value: 'history',
-  label: 'História',
+  label: 'história',
   color: 'bg-orange-500'
 }, {
   value: 'news',
-  label: 'Notícias',
-  color: 'bg-teal-500'
+  label: 'notícia',
+  color: 'bg-violet-500'
 }];
