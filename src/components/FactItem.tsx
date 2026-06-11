c92c1d2bce81f6d9df143e241482ccdebb2f8762
luc-fr <lucfr1991@gmail.com>
2026-06-11 11:40:41 +0000

    refactory: criar logica de exibicao de etiquetas de categorias em portugues

--- src/components/FactItem.tsx
+++ src/components/FactItem.tsx
@@ -1,16 +1,19 @@
 import type { Fact } from "../types";
+import { CATEGORIES } from "../constants";
 
 interface FactItemProps {
   fact: Fact;
 };
 
 export default function FactItem({ fact }: FactItemProps) {
+  const category = CATEGORIES.find(object => fact.category === object.value);
+
   return (
     <>
       <li>
         <p>{ fact.text }</p>
-        <a href={ fact.source } target="_blank">Fonte</a>
-        <span>{ fact.category }</span>
+        <a href={ fact.source } target="_blank">(fonte)</a>
+        <span>{ category?.label }</span>
       </li>
     </>
   );
