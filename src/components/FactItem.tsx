f30ec4a5e99aa04df1da38c0ab51ddf886b2bf68
luc-fr <lucfr1991@gmail.com>
2026-06-18 18:13:28 +0000

    refactory: alterar estrutura de marcacao

--- src/components/FactItem.tsx
+++ src/components/FactItem.tsx
@@ -10,10 +10,18 @@ export default function FactItem({ fact }: FactItemProps) {
 
   return (
     <>
-      <li>
-        <p>{ fact.text }</p>
-        <a href={ fact.source } target="_blank">(fonte)</a>
-        <span>{ category?.label }</span>
+      <li className="bg-stone-700 px-6 py-4 flex justify-between items-center gap-4 rounded-2xl">
+        <p className="text-[20px]">
+          {fact.text}
+          <a
+            href={fact.source}
+            target="_blank"
+            className="text-stone-400 text-[20px] capitalize hover:text-blue-500 transition duration-150"
+          > (fonte)</a>
+        </p>
+        <span
+          className={`${category?.color} text-[14px] uppercase pt-0.75 px-2.5 rounded-full `}
+        >{category?.label}</span>
       </li>
     </>
   );
