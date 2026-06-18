75fbdb561f510dbae27292c63e45fcb3ee361e20
luc-fr <lucfr1991@gmail.com>
2026-06-18 18:56:20 +0000

    style: adicionar estilizacao completa

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,9 +8,18 @@ export default function Header(
 ) {
   return (
     <>
-      <header>
-        <h1>hoje eu aprendi</h1>
-        <button onClick={onToggleForm}>
+      <header className="flex justify-between mb-10">
+        <div className="flex items-center gap-4">
+          <img
+            src="./logo.png"
+            className="w-17"
+          />
+          <h1 className="text-[42px] uppercase leading-none mt-1.5">hoje eu aprendi</h1>
+        </div>
+        <button
+          onClick={onToggleForm}
+          className="h-14.25 bg-linear-[135deg,#3b82f6,#ef4444,#16a34a,#eab308] text-[20px] pt-5 px-8 pb-4.25 btn"
+        >
           { showForm ? 'fechar' : 'compartilhar um fato' }
         </button>
       </header>
